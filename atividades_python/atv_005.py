5237b9c0d8a783d26830692d6da6428b3397dbb2
Ricardo <sousasilvaricardo9@gmail.com>
2024-10-17 00:28:38 -0400

    alteração atv 5

--- atividades_python/atv_005.py
+++ atividades_python/atv_005.py
@@ -12,5 +12,18 @@
 
 #função para verificar se um numero é primo
 
-def primo():
+def primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numero=int(input("Digite um número: ")) 
+if primo(numero):
+    print(f"{numero} é um número primo.")
+else:
+    print(f"{numero} não é um número primo.")
+
      
